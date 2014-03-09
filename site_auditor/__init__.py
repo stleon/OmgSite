7b6746b0c9c5aa203cee686bc9ce1b82470f5dee
ST LEON <leonst998@gmail.com>
2014-03-09 23:45:41 +0400

    v1.3.5
    багфиксы, адаптация под saw

--- site_auditor/__init__.py
+++ site_auditor/__init__.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 __author__ = 'ST LEON'
 __title__ = 'site-auditor'
-__version__ = '1.3.4'
+__version__ = '1.3.5'
 __license__ = 'GPLv3'
 __copyright__ = 'Copyright 2014 ST LEON'
 
